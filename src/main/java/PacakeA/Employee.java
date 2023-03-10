c0d19d52901a61ebad51511f7f1339bafdd72916
Sean Veasna <veasnavk711@gmail.com>
2023-03-10 14:49:42 +0700

    add employeeCls

---
+++ src/main/java/PacakeA/Employee.java
@@ -0,0 +1,9 @@
+package PacakeA;
+
+public class Employee {
+    // instance variable
+    protected String name;
+    protected int age ;
+    protected   String gender;
+    String address;
+}
---
+++ src/main/java/PacakeB/ClsExtends.java
@@ -0,0 +1,16 @@
+package PacakeB;
+
+import PacakeA.Employee;
+
+public class ClsExtends extends Employee {
+    public static void main(String[] args) {
+        ClsExtends newExtend = new ClsExtends();
+        newExtend.name="veasna";
+        newExtend.gender="Male";
+        newExtend.age=39;
+        System.out.println(newExtend.name);
+        System.out.println(newExtend.age);
+        System.out.println(newExtend.gender);
+
+    }
+}
